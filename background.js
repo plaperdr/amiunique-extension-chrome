3be2b3df461453dfd21009b9b7eaeffe5f5bf13d
Pierre Laperdrix <pierre.laperdrix@inria.fr>
2015-08-12 14:47:36 +0200

    Adding information panel and bumped to version 0.2

--- background.js
+++ background.js
@@ -3,7 +3,12 @@
 console.log("AmIUnique extension");
 
 var iframe;
+
+//Extension variables
 var uuid;
+var nbEvol;
+var lastSent;
+var changesToSee;
 
 function generateUUID(){
     var d = new Date().getTime();
@@ -30,7 +35,9 @@ function registerListener(){
     iframe.addEventListener("load", function () {
         if(!iframe.src.startsWith("chrome-extension")) {
 
-            chrome.runtime.sendMessage({uuid: uuid}, function(response) {
+            chrome.runtime.sendMessage({
+                from: "background",
+                uuid: uuid
             });
 
             setTimeout(clearIframe,10000);
@@ -52,19 +59,56 @@ function startLoop(){
     setInterval(loadIframe,4*60*60*1000);
 }
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-    console.log(request);
-});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+
+        console.log(request);
+
+        if (request.from == "content") {
+            var newEvol = parseInt(request.nbEvol);
+
+            //If the fingerprint has changed
+            //We indicate it in the browser action
+            if (newEvol > nbEvol) {
+                chrome.browserAction.setBadgeText({text:"!"});
+                nbEvol = newEvol;
+                changesToSee = true;
+                chrome.storage.local.set({'nbEvol': nbEvol, 'changesToSee':true});
+            }
+
+            //We update the time the last FP was sent
+            lastSent = new Date();
+            chrome.storage.local.set({'lastSent': lastSent});
+
+        } else if (request.from == "popup"){
+
+            //If the user has clicked on the "View changes" button
+            //We reset the text the badge of the browser action
+            chrome.browserAction.setBadgeText({text:""});
+            changesToSee = false;
+            chrome.storage.local.set({'changesToSee':false});
+
+        }
+    }
+);
 
 
 //Get the unique ID in Chrome storage
 //If not present, generate it
-chrome.storage.local.get('uuid',function(items) {
-    uuid = items.uuid;
+chrome.storage.local.get(function(items) {
+    //uuid = items.uuid;
+    uuid = undefined;
+    nbEvol = items.nbEvol;
+    changesToSee = items.changesToSee;
+    lastSent = items.lastSent;
     if(uuid === undefined){
         uuid = generateUUID();
-        chrome.storage.local.set({'uuid': uuid});
+        nbEvol = 0;
+        changesToSee = false;
+        lastSent = new Date();
+        chrome.storage.local.set({'uuid': uuid, 'nbEvol':nbEvol, 'changesToSee':changesToSee, 'lastSent': lastSent});
     }
+    if(changesToSee) chrome.browserAction.setBadgeText({text:"!"});
 });
 
 //Start the main application loop when the background document is ready
--- content.js
+++ content.js
@@ -3,9 +3,23 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 
-        //Adding the UUID in the loaded iframe
-        fp = document.getElementById("fp");
-        fp.setAttribute("uuid", request.uuid);
-        
+        if(request.from == "background") {
+
+            //Adding the UUID in the loaded iframe
+            fp = document.getElementById("fp");
+            fp.setAttribute("uuid", request.uuid);
+
+            setTimeout(function () {
+                numb = fp.getAttribute("nbEvol");
+
+                chrome.runtime.sendMessage({
+                    from: 'content',
+                    subject: 'nbEvol',
+                    nbEvol: numb
+                });
+
+            }, 5000);
+        }
+
     }
 );
